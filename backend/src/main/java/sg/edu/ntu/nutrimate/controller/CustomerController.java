0e544c437574557f1e3edc8d247c1d334aae88c0
Jerome Leong <jerome.leong@gmail.com>
2023-07-25 18:58:04 +0800

    update Dockerfile, CustomerController and config.yml

--- backend/src/main/java/sg/edu/ntu/nutrimate/controller/CustomerController.java
+++ backend/src/main/java/sg/edu/ntu/nutrimate/controller/CustomerController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import sg.edu.ntu.nutrimate.entity.Customer;
 import sg.edu.ntu.nutrimate.entity.PasswordDataObject;
 import sg.edu.ntu.nutrimate.service.CustomerService;
 
+@CrossOrigin(origins={"http://localhost:3000/"}, allowCredentials = "true")
 @RestController
 @RequestMapping("/nutrimate")
 public class CustomerController {
