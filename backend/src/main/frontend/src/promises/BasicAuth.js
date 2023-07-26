1b1a11efbf6ee630ca33ac21ed485dec84124f43
Jerome Leong <jerome.leong@gmail.com>
2023-07-26 22:35:17 +0800

    update config.yml and react files for deployment

--- backend/src/main/frontend/src/promises/BasicAuth.js
+++ backend/src/main/frontend/src/promises/BasicAuth.js
@@ -1,8 +1,15 @@
 import axios from  "axios";
 
+//For Development
+// var config = {
+//   // baseURL: 'https://localhost:8443/',
+//   baseURL: 'http://localhost:8080/',    
+// };
+
+//For Production
 var config = {
   // baseURL: 'https://localhost:8443/',
-  baseURL: 'http://localhost:8080/',    
+  // baseURL: 'http://localhost:8080/',    
 };
 
 const BasicAuth = axios.create(config);
